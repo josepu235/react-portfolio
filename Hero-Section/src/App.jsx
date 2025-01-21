8c00e4494d54bee21e14b36109af142493c2c99d
josepu235 <sogradiel123@gmail.com>
2025-01-21 23:59:36 +0800

    First Commit

---
+++ Hero-Section/src/App.jsx
@@ -0,0 +1,24 @@
+import pp from "./assets/ppz.png"
+import './App.css'
+
+function App() {
+
+  return (
+
+
+    <section className="Hero-prof">
+      <div className="Hero-pic">
+        <img src={pp} alt="Josef profile picture" />
+      </div>
+      <div className="Hero-text">
+        <h2 className="Hero-intro">Hello, I'm</h2>
+        <h1 className="Hero-name">Josef Nathan Sogradiel</h1>
+        <p className="Hero-desc">Josef Nathan is the main protagonist in the popular manga and anime series Naruto. He is a cheerful, hyperactive, strong-willed, and occasionally simple-minded young shinobi from the village of Konoha.</p>
+          
+        </div>
+
+      </section>
+  )
+}
+
+export default App
